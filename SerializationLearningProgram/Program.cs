b67e53a3a4c396a26366ab7835765b326c64cd18
Vishwas <baghel.vishwas0@gmail.com>
2022-07-13 11:25:23 +0530

    [Vishwas]add: Serialization Learning Program Added

---
+++ SerializationLearningProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SerializationLearningProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Serialization Learning Program\n*********************************************");
+        }
+    }
+}
